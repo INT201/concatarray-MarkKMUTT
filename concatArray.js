bcae07548d27ba2e0425f411643b3f7b480ab2e9
MarkKMUTT <88104343+MarkKMUTT@users.noreply.github.com>
2022-09-09 19:21:29 +0700

    Update concatArray.js

--- concatArray.js
+++ concatArray.js
@@ -1,17 +1,15 @@
-const { template } = require('@babe/core')
+const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-    //code here
-    if (array1 === undefined || array2 === undefined || array1 === null || array2 === null) {
-        return undefined;
-    } else if (array1.length === 0 && array2.length === 0) {
-        return [];
-    } else if (array1.length === 0) {
-        return array2;
-    } else if (array2.length === 0) {
-        return array1;
-    } else {
-        return [...array1, ...array2];
-    }
+  //code here
+  if (array1 == null && array2 == null) {
+    return undefined
+  } else if (array1 == null) {
+    return array2
+  } else if (array2 == null) {
+    return array1
+  } else {
+    return array1.concat(array2)
+  }
 }
-module.exports = concatArray;
+module.exports = concatArray
